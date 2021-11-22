5ae8fe4f463c61c951c5683b7c43d5069d48dcf2
pajic <fica.pajic@gmail.com>
2021-11-22 11:59:38 +0100

    Added tend functionality

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { ethers, BigNumber } = require('ethers');
 const { strategies, provider } = require('./strategies');
 const { getGasPrice } = require('./gas');
 const Sentry = require('@sentry/node');
+const { METHODS_TO_EXECUTE } = require('./constants');
 
 const callCost = BigInt(10000000000000000); // in wei
 
@@ -12,16 +13,29 @@ async function harvestTrigger(strategy, callCostInWei) {
 	return await strategy.harvestTrigger(callCostInWei);
 }
 
-async function harvest(contract) {
+async function tendTrigger(strategy, callCostInWei) {
+	return await strategy.tendTrigger(callCostInWei);
+}
+
+/**
+ * @param contract Strategy contract instance
+ * @param method ENUM which method to execute from METHODS_TO_EXECUTE
+ * */
+async function execute(contract, method) {
 	const strategy = await contract.connect(signer);
+	const isMethodHarvest = method === METHODS_TO_EXECUTE.harvest;
 
 	const sentryTransaction = Sentry.startTransaction({
-		op: 'pre-harvest',
-		name: 'Catch if Harvest would fail',
+		op: 'pre-' + method,
+		name: 'Catch if ' + method + ' would fail',
 	});
 
 	try {
-		await strategy.callStatic.harvest();
+		if (isMethodHarvest) {
+			await strategy.callStatic.harvest();
+		} else {
+			await strategy.callStatic.tend();
+		}
 	} catch (error) {
 		Sentry.captureException(error);
 		console.error('Error occured, check Sentry for details');
@@ -29,7 +43,12 @@ async function harvest(contract) {
 	}
 
 	// estimate the gas of transaction
-	const estimatedGas = await strategy.estimateGas.harvest();
+	let estimatedGas = 0;
+	if (isMethodHarvest) {
+		estimatedGas = await strategy.estimateGas.harvest()
+	} else {
+		estimatedGas = await strategy.estimateGas.tend()
+	}
 
 	// mutliple that value with 1.3
 	const gasLimit = estimatedGas.add(
@@ -41,11 +60,18 @@ async function harvest(contract) {
 
 	const nonce = await signer.getTransactionCount();
 
-	const transaction = await strategy.harvest({
+	const txParams = {
 		gasLimit: gasLimit,
 		gasPrice: gasPrice,
 		nonce: nonce
-	});
+	}
+
+	let transaction;
+	if (isMethodHarvest) {
+		transaction = await strategy.harvest(txParams);
+	} else {
+		transaction = await strategy.tend(txParams);
+	}
 
 	console.log(`Tx Hash: ${transaction.hash}`);
 	console.log(`Waiting for the transaction to be mined...`);
@@ -53,43 +79,52 @@ async function harvest(contract) {
 	console.log(`Transaction confirmed!\n`);
 }
 
-async function main() {
+async function main(method) {
+	const isMethodHarvest = method === METHODS_TO_EXECUTE.harvest;
+
 	const sentryTransaction = Sentry.startTransaction({
-		op: 'harvest',
-		name: 'Catch Harvest failed transactions',
+		op: method,
+		name: 'Catch ' + method + ' failed transactions',
 	});
 
-	console.log(`Check for harvest started...\n`);
+	console.log('Check for ' + method + ' started...\n');
 
 	for (const [contract, name] of strategies.entries()) {
 		console.log(`Checking: ${name}`);
-		const shouldHarvest = await harvestTrigger(contract, callCost);
-		if (shouldHarvest) {
-			console.log(`Trying to harvest...`);
+		let shouldExecute = false;
+
+		if (isMethodHarvest) {
+			shouldExecute = await harvestTrigger(contract, callCost);
+		} else {
+			shouldExecute = await tendTrigger(contract, callCost);
+		}
+
+		if (shouldExecute) {
+			console.log('Trying to ' + method + '...');
 			try {
-				await harvest(contract);
+				await execute(contract, method)
 			} catch (error) {
 				Sentry.captureException(error);
 				console.error('Error occured, check Sentry for details');
 			}
 		} else {
-			console.log(`Strategy doesn't need harvesting\n`);
+			console.log('Strategy doesn\'t need ' + method + '\n');
 		}
 	}
 
 	sentryTransaction.finish();
 }
 
-function exec() {
+function exec(method, interval) {
 	const sentryTransaction = Sentry.startTransaction({
 		op: 'general',
 		name: 'Catch general errors',
 	});
 
-	main()
+	main(method)
 		.then(() =>
 			console.log(
-				`Harvest check done, next in ${process.env.CHECK_INTERVAL} miliseconds\n`
+				`Harvest check done, next in ${interval} miliseconds\n`
 			)
 		)
 		.catch((error) => {
@@ -106,6 +141,10 @@ Sentry.init({
 
 console.log('🚀 Keeper bot script is up & running\n');
 
-exec();
+const harvestInterval = process.env.HARVEST_CHECK_INTERVAL;
+const tendInterval = process.env.TEND_CHECK_INTERVAL;
 
-setInterval(() => exec(), process.env.CHECK_INTERVAL);
+exec(METHODS_TO_EXECUTE.harvest, harvestInterval);
+
+setInterval(() => exec(METHODS_TO_EXECUTE.harvest, harvestInterval), harvestInterval);
+setInterval(() => exec(METHODS_TO_EXECUTE.tend, tendInterval), tendInterval);
