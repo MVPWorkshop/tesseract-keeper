aecf7a7bab1d5dbd9bacc087cd337672b1e2e598
Filip Pajic <42151917+pajicf@users.noreply.github.com>
2021-11-26 15:36:23 +0100

    Added tend functionality (#4)
    * Added tend functionality
    
    * Added tend functionality
    
    * Added new strategies

--- strategies.js
+++ strategies.js
@@ -13,37 +13,32 @@ const strategyGenLevAaveDaiAddress = `0x901f316658Fb49F54de72eE2621C1789074D37E9
 const strategyGenLevAaveWbtcAddress = `0xE6951cef4968F03D42A6c30683F102A5cEb59B82`;
 const strategyGenLevAaveWethAddress = `0x623a24146F65d7Bf68e47b355d92FE84190F30C0`;
 
-// Contract instances
-const leveragedAaveUsdc = new ethers.Contract(
-	strategyGenLevAaveUsdcAddress,
-	strategyGenLevAaveAbi,
-	provider
-);
+// v0.4.3.1
+const strategyGenLevAaveV2UsdcAddress = `0xb1A092293290E60B288B2B75D83a1a086392C037`;
+const strategyGenLevAaveV2DaiAddress = `0xb32A63f4F713Ab83a23d8c464e2a1a5AF10Acb89`;
+const strategyGenLevAaveV2WbtcAddress = `0x3790283EF6cDc310EA42F3C98039444aB9195c16`;
+const strategyGenLevAaveV2WethAddress = `0x9552efE8985749C0A292926dfF50f8895f19A3ED`;
+const strategyGenLevAaveV2WmaticAddress = `0x85486d270Aa0329978F1EFbba4e807E489a86260`;
 
-const leveragedAaveDai = new ethers.Contract(
-	strategyGenLevAaveDaiAddress,
-	strategyGenLevAaveAbi,
-	provider
-);
-
-const leveragedAaveWbtc = new ethers.Contract(
-	strategyGenLevAaveWbtcAddress,
-	strategyGenLevAaveAbi,
-	provider
-);
-
-const leveragedAaveWeth = new ethers.Contract(
-	strategyGenLevAaveWethAddress,
-	strategyGenLevAaveAbi,
-	provider
-);
+const leverageAaveFactory = (strategyAddress) => {
+	return new ethers.Contract(
+		strategyAddress,
+		strategyGenLevAaveAbi,
+		provider
+	);
+}
 
 // Export strategies
 const strategies = new Map([
-	[leveragedAaveUsdc, 'Leveraged AAVE - USDC Strategy'],
-	[leveragedAaveDai, 'Leveraged AAVE - DAI Strategy'],
-	[leveragedAaveWbtc, 'Leveraged AAVE - WBTC Strategy'],
-	[leveragedAaveWeth, 'Leveraged AAVE - WETH Strategy'],
+	[leverageAaveFactory(strategyGenLevAaveUsdcAddress), 'Leveraged AAVE - USDC Strategy'],
+	[leverageAaveFactory(strategyGenLevAaveDaiAddress), 'Leveraged AAVE - DAI Strategy'],
+	[leverageAaveFactory(strategyGenLevAaveWbtcAddress), 'Leveraged AAVE - WBTC Strategy'],
+	[leverageAaveFactory(strategyGenLevAaveWethAddress), 'Leveraged AAVE - WETH Strategy'],
+	[leverageAaveFactory(strategyGenLevAaveV2UsdcAddress), 'Leveraged AAVE V2 - USDC Strategy'],
+	[leverageAaveFactory(strategyGenLevAaveV2DaiAddress), 'Leveraged AAVE V2 - DAI Strategy'],
+	[leverageAaveFactory(strategyGenLevAaveV2WbtcAddress), 'Leveraged AAVE V2 - WBTC Strategy'],
+	[leverageAaveFactory(strategyGenLevAaveV2WethAddress), 'Leveraged AAVE V2 - WETH Strategy'],
+	[leverageAaveFactory(strategyGenLevAaveV2WmaticAddress), 'Leveraged AAVE V2 - WMATIC Strategy'],
 ]);
 
 module.exports = {
